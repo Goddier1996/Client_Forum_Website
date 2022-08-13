258d167cfcfa94caadbb214faaf80ac9cd990482
artium_96 <artium20@gmail.com>
2022-08-13 15:19:13 +0300

    change

--- src/pages/home.js
+++ src/pages/home.js
@@ -31,7 +31,7 @@ const Home = () => {
     Swal.fire({
       background: 'none',
       showConfirmButton: false,
-      timer: 5000,
+      timer: 8000,
       html: '<div class="popUpHome"> <h1>Welcome to Community Forum<h1> <p>( please wait connect to server )</p>  <img src="https://i.pinimg.com/originals/e3/1b/75/e31b752875679b64fce009922f9f0dda.gif"> </div>'
     })
 
