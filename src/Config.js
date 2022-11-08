ae01d1e96a01d8b54091392658dce88fb3ddf06b
artium_96 <artium20@gmail.com>
2022-11-08 18:20:29 +0200

    chnageWebServerApi

--- src/Config.js
+++ src/Config.js
@@ -1,9 +1,23 @@
 // API ייצוא כתובת השרת לדף 
+// export const Config = {
+
+
+//     API: {
+//         BASE: "https://servforum.herokuapp.com",
+//     }
+
+
+// }
+
+
+
+
+
 export const Config = {
 
 
     API: {
-        BASE: "https://servforum.herokuapp.com",
+        BASE: "https://serverwebsiteforumnodejs-production.up.railway.app",
     }
 
 
